56576c7fa706f4ad54fb37d6a934e14eb4726f38
KMV <9117479@gmail.com>
2023-01-03 15:56:59 +0300

    js 030123

--- js-course/assets/js/main.js
+++ js-course/assets/js/main.js
@@ -7,3 +7,39 @@ document.querySelector('.inner').addEventListener('click', (event) =>{
 document.querySelector('.close').addEventListener('click', (event) =>{
     document.body.classList.remove('is-menu-visible');
 });
+
+function validate(event) {
+
+    const errorContainer = document.getElementById('errorsForm');
+    errorContainer.innerHTML ='';
+    const nameField = document.getElementById('name'),
+    emailField = document.getElementById('email'),
+    messageField = document.getElementById('message'),
+    re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    let validForm = true;
+
+    if (! nameField.value || nameField.value.length < 3) {
+        errorContainer.innerHTML = '<p style="color:red"><strong>Name error!</strong></p>';
+        validForm = false;
+    }
+
+    if (! emailField.value || ! re.test(String(emailField.value).toLowerCase()) ) {
+        errorContainer.innerHTML += '<p style="color:red"><strong>Email error!</strong></p>';
+        validForm = false;
+    }
+
+    if (! messageField.value || messageField.value.length < 10) {
+        errorContainer.innerHTML += '<p style="color:red"><strong>Message error!</strong></p>';
+        validForm = false;
+    }
+
+    if (validForm === false) {
+        event.preventDefault();
+    }
+    
+    return validForm;
+}
+
+const formQuestion = document.getElementById('contactform');
+
+formQuestion.addEventListener('submit', validate);
