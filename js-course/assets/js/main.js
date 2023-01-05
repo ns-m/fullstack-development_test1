4229e3f2809f103290cf59e96e847d2c698dd3e2
KMV <9117479@gmail.com>
2023-01-05 16:39:19 +0300

    LocalStorage 2

--- js-course/assets/js/main.js
+++ js-course/assets/js/main.js
@@ -16,26 +16,36 @@ function validate(event) {
     emailField = document.getElementById('email'),
     messageField = document.getElementById('message'),
     re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    
+    const contactFormData = {
+        name : '',
+        email : '',
+    };
+
     let validForm = true;
 
     if (! nameField.value || nameField.value.length < 3) {
         errorContainer.innerHTML = '<p style="color:red"><strong>Name error!</strong></p>';
         validForm = false;
     }else{
-        localStorage.setItem('name', nameField.value);
+        contactFormData.name = nameField.value;
+        //localStorage.setItem('name', nameField.value);
     }
 
     if (! emailField.value || ! re.test(String(emailField.value).toLowerCase()) ) {
         errorContainer.innerHTML += '<p style="color:red"><strong>Email error!</strong></p>';
         validForm = false;
     }else{
-        localStorage.setItem('email', emailField.value);
+        contactFormData.email = emailField.value;
+        //localStorage.setItem('email', emailField.value);
     }
 
     if (! messageField.value || messageField.value.length < 10) {
         errorContainer.innerHTML += '<p style="color:red"><strong>Message error!</strong></p>';
         validForm = false;
     }
+
+    localStorage.setItem('contactFormData', JSON.stringify(contactFormData));
 
     if (validForm === false) {
         event.preventDefault();
@@ -48,10 +58,16 @@ const formQuestion = document.getElementById('contactform');
 
 formQuestion.addEventListener('submit', validate);
 
-const nameField = document.getElementById('name');
-const userName = localStorage.getItem('name');
-nameField.value = userName;
+// const nameField = document.getElementById('name');
+// const userName = localStorage.getItem('name');
+// nameField.value = userName;
 
-const emailField = document.getElementById('email');
-const emailName = localStorage.getItem('email');
-emailField.value = emailName;
+// const emailField = document.getElementById('email');
+// const emailName = localStorage.getItem('email');
+// emailField.value = emailName;
+
+const contactFormData = JSON.parse(localStorage.getItem('contactFormData')),
+    nameField = document.getElementById('name'),
+    mailField = document.getElementById('email');
+nameField.value = contactFormData.name;
+mailField.value = contactFormData.email;
