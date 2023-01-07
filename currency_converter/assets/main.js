80e44864c4a1eb75c6af71cabb821827097c2b2f
KMV <9117479@gmail.com>
2023-01-07 20:16:14 +0300

    currency_converter fin

--- currency_converter/assets/main.js
+++ currency_converter/assets/main.js
@@ -12,15 +12,23 @@ var requestOptions = {
 let myRates;
 
 let data = fetch("https://api.apilayer.com/fixer/convert?to=RUB&from=EUR&amount=10", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result.valueOf))
-  .catch(error => console.log('error', error));  
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+    myRates = result.info.rate;
+    //console.log(myRates);
+    })
+  .catch(error => console.log('error', error))  
+  ;  
+
+
 
 const cur1 = document.getElementById('cur1');
 const cur2 = document.getElementById('cur2');
 
 function changeCur(event) {
-    console.log('change filder: ' + event.target.value);
+    //console.log('change filder: ' + event.target.value);
+    cur2.value = parseFloat(event.target.value / myRates).toFixed(2);
 }
 
 // cur1.addEventListener('keyup', (event) =>{
@@ -32,5 +40,12 @@ function changeCur(event) {
 // });
 
 cur1.addEventListener('keyup', changeCur);
-
 cur1.addEventListener('mouseup', changeCur);
+
+function changeCur2(event) {
+    //console.log('change filder: ' + event.target.value);
+    cur1.value = parseFloat(event.target.value * myRates).toFixed(2);
+}
+
+cur2.addEventListener('keyup', changeCur2);
+cur2.addEventListener('mouseup', changeCur2);
